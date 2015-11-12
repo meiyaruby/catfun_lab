8a05a02a73e7d1e5ca5472869e55308041ce34bf
Meiya Chang <hc6uj@virginia.edu>
2015-11-12 18:49:52 -0500

    added simulation files

---
+++ simulation/catGo.py
@@ -0,0 +1,53 @@
+# from trepan.api import debug
+
+# import pdb
+# pdb.set_trace()
+
+import runWorld as rw
+import drawWorld as dw
+
+
+# Initialize world
+name = "Cat Go!"
+width = 500
+height = 500
+rw.newDisplay(width, height, name)
+
+# World state will be single x coordinate at left edge of world
+initState = 0
+
+# Display the state by drawing a cat at that x coordinate
+myimage = dw.loadImage("cat.bmp")
+
+# state -> image (IO)
+def updateDisplay(state):
+    dw.fill(dw.black)
+    dw.draw(myimage, (state, height/2))
+
+
+# We'll update the state on each tick by incrementing the x stateinate
+
+# state -> state 
+def updateState(state):
+    return(state+1)
+
+# We'll terminate when the x stateinate reaches the screen edge
+
+# state -> bool
+def endState(state):
+    if (state >= width):
+        return True
+    else:
+        return False
+
+
+# For now we'll handle events just logging them to the console
+# state -> event -> state
+def handleEvent(state, event):
+    return(state)
+
+# Off we go! Start the cat at the left edge, and try for 30 FPS
+frameRate = 60
+initState = 0
+rw.runWorld(initState, updateDisplay, updateState, handleEvent,
+            endState, frameRate)
